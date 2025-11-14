2ca6bc14a216c674645959b5ec8cc34f374e2be1
Shubham Shah <shahshubham1888@gmail.com>
2025-11-15 04:19:13 +0530

    Add HTTPS configuration with self-signed certificate and private key

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from 'fs'; // Import Node.js file system module
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    https: {
+      key: fs.readFileSync('./localhost-key.pem'), // Path to your private key
+      cert: fs.readFileSync('./localhost.pem'),   // Path to your certificate
+    },
+  },
 })
